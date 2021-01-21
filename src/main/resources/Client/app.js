2c40217c3cb4542c1873bd5d9f0506b0fe6932bc
Amr Elghobary <amr.elghobary@gmail.com>
2021-01-21 18:41:24 +0200

    add sending message to socket.

--- src/main/resources/Client/app.js
+++ src/main/resources/Client/app.js
@@ -6,6 +6,9 @@ var socketStatus = document.getElementById("status");
 var closeBtn = document.getElementById("close");
 var refreshBtn = document.getElementById("refresh");
 
+var sentMessagesCount = 0;
+const sendBtn = document.getElementById("send");
+
 startWebsocket();
 
 function startWebsocket() {
@@ -13,26 +16,30 @@ function startWebsocket() {
   // Handle messages sent by the server.
   // Show a connected message when the WebSocket is opened.
   socket.onopen = function (event) {
-    socketStatus.innerHTML = "Connected";
+    socketStatus.innerHTML = "CONNECTED";
     socketStatus.className = "open";
   };
+
   socket.onmessage = function (event) {
     var message = event.data;
     console.log(JSON.parse(message));
-    messagesList.innerHTML +=
-      '<li class="received"><span>Received:</span>' + message + "</li>";
+    messagesList.innerHTML = 
+      '<li class="received"><span>Received:</span>' + message + "</li>" + messagesList.innerHTML;
   };
+
   // Handle any errors that occur.
   socket.onerror = function (error) {
     socket = null;
     console.log("WebSocket Error: " + error);
   };
+
   socket.onclose = function () {
     socket = null;
     socketStatus.innerHTML = "Disconnected";
     socketStatus.className = "closed";
-    setTimeout(startWebsocket,1000);
+    setTimeout(startWebsocket, 1000);
   };
+
   // Close the WebSocket connection when the close button is clicked.
   closeBtn.onclick = function (e) {
     e.preventDefault();
@@ -42,9 +49,20 @@ function startWebsocket() {
 
     return false;
   };
+
   refreshBtn.onclick = function (e) {
+    console.log(e);
+
     e.preventDefault();
     startWebsocket();
   };
+
+  sendBtn.onclick = function (e) {
+    console.log(e);
+    e.preventDefault();
+    socket.send('message: #' + sentMessagesCount);
+
+    sentMessagesCount += 1;
+  }
 }
 
